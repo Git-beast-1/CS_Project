027c6ec32c4441958444db35d08b922ce8a49f1b
ayoay <ayoay@ANDRONICUS>
2025-09-09 01:12:51 +0100

    Add project files.

---
+++ CS_Course_Work/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CS_Course_Work
+{
+    partial class F_Question_Template
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.T_Question = new System.Windows.Forms.RichTextBox();
+            this.T_Op_1 = new System.Windows.Forms.RichTextBox();
+            this.T_Op_2 = new System.Windows.Forms.RichTextBox();
+            this.T_Op_3 = new System.Windows.Forms.RichTextBox();
+            this.T_Op_4 = new System.Windows.Forms.RichTextBox();
+            this.T_Op_5 = new System.Windows.Forms.RichTextBox();
+            this.T_Op_6 = new System.Windows.Forms.RichTextBox();
+            this.But_Create_Quiz = new System.Windows.Forms.Button();
+            this.But_New_Question = new System.Windows.Forms.Button();
+            this.T_Right_Answer = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.T_Timer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // T_Question
+            // 
+            this.T_Question.Location = new System.Drawing.Point(81, 71);
+            this.T_Question.Name = "T_Question";
+            this.T_Question.Size = new System.Drawing.Size(530, 315);
+            this.T_Question.TabIndex = 0;
+            this.T_Question.Text = "";
+            // 
+            // T_Op_1
+            // 
+            this.T_Op_1.Location = new System.Drawing.Point(81, 426);
+            this.T_Op_1.Name = "T_Op_1";
+            this.T_Op_1.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_1.TabIndex = 1;
+            this.T_Op_1.Text = "";
+            // 
+            // T_Op_2
+            // 
+            this.T_Op_2.Location = new System.Drawing.Point(509, 426);
+            this.T_Op_2.Name = "T_Op_2";
+            this.T_Op_2.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_2.TabIndex = 2;
+            this.T_Op_2.Text = "";
+            // 
+            // T_Op_3
+            // 
+            this.T_Op_3.Location = new System.Drawing.Point(920, 426);
+            this.T_Op_3.Name = "T_Op_3";
+            this.T_Op_3.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_3.TabIndex = 3;
+            this.T_Op_3.Text = "";
+            // 
+            // T_Op_4
+            // 
+            this.T_Op_4.Location = new System.Drawing.Point(81, 573);
+            this.T_Op_4.Name = "T_Op_4";
+            this.T_Op_4.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_4.TabIndex = 4;
+            this.T_Op_4.Text = "";
+            // 
+            // T_Op_5
+            // 
+            this.T_Op_5.Location = new System.Drawing.Point(509, 573);
+            this.T_Op_5.Name = "T_Op_5";
+            this.T_Op_5.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_5.TabIndex = 5;
+            this.T_Op_5.Text = "";
+            // 
+            // T_Op_6
+            // 
+            this.T_Op_6.Location = new System.Drawing.Point(920, 564);
+            this.T_Op_6.Name = "T_Op_6";
+            this.T_Op_6.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_6.TabIndex = 6;
+            this.T_Op_6.Text = "";
+            // 
+            // But_Create_Quiz
+            // 
+            this.But_Create_Quiz.Location = new System.Drawing.Point(679, 71);
+            this.But_Create_Quiz.Name = "But_Create_Quiz";
+            this.But_Create_Quiz.Size = new System.Drawing.Size(177, 90);
+            this.But_Create_Quiz.TabIndex = 10;
+            this.But_Create_Quiz.Text = "Create Quiz";
+            this.But_Create_Quiz.UseVisualStyleBackColor = true;
+            // 
+            // But_New_Question
+            // 
+            this.But_New_Question.Location = new System.Drawing.Point(899, 71);
+            this.But_New_Question.Name = "But_New_Question";
+            this.But_New_Question.Size = new System.Drawing.Size(177, 90);
+            this.But_New_Question.TabIndex = 11;
+            this.But_New_Question.Text = "New Question";
+            this.But_New_Question.UseVisualStyleBackColor = true;
+            this.But_New_Question.Click += new System.EventHandler(this.But_New_Question_Click);
+            // 
+            // T_Right_Answer
+            // 
+            this.T_Right_Answer.Location = new System.Drawing.Point(509, 715);
+            this.T_Right_Answer.Name = "T_Right_Answer";
+            this.T_Right_Answer.Size = new System.Drawing.Size(323, 72);
+            this.T_Right_Answer.TabIndex = 12;
+            this.T_Right_Answer.Text = "Input the correct Answer";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 105);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "L";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1255, 344);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 105);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "R";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // T_Timer
+            // 
+            this.T_Timer.Location = new System.Drawing.Point(679, 188);
+            this.T_Timer.Name = "T_Timer";
+            this.T_Timer.Size = new System.Drawing.Size(177, 88);
+            this.T_Timer.TabIndex = 15;
+            this.T_Timer.Text = "Enter Time in seconds";
+            // 
+            // F_Question_Template
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1330, 901);
+            this.Controls.Add(this.T_Timer);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.T_Right_Answer);
+            this.Controls.Add(this.But_New_Question);
+            this.Controls.Add(this.But_Create_Quiz);
+            this.Controls.Add(this.T_Op_6);
+            this.Controls.Add(this.T_Op_5);
+            this.Controls.Add(this.T_Op_4);
+            this.Controls.Add(this.T_Op_3);
+            this.Controls.Add(this.T_Op_2);
+            this.Controls.Add(this.T_Op_1);
+            this.Controls.Add(this.T_Question);
+            this.Name = "F_Question_Template";
+            this.Text = "Question Template";
+            this.Load += new System.EventHandler(this.F_Question_Template_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox T_Question;
+        private System.Windows.Forms.RichTextBox T_Op_1;
+        private System.Windows.Forms.RichTextBox T_Op_2;
+        private System.Windows.Forms.RichTextBox T_Op_3;
+        private System.Windows.Forms.RichTextBox T_Op_4;
+        private System.Windows.Forms.RichTextBox T_Op_5;
+        private System.Windows.Forms.RichTextBox T_Op_6;
+        private System.Windows.Forms.Button But_Create_Quiz;
+        private System.Windows.Forms.Button But_New_Question;
+        private System.Windows.Forms.RichTextBox T_Right_Answer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RichTextBox T_Timer;
+    }
+}
+
---
+++ CS_Course_Work/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_Course_Work
+{
+    public partial class F_Question_Template : Form
+    {
+            int I_Is_pair;
+        public class Question_Setup
+        {
+            public string Question;
+            public List<string> Options;
+            public string Right_Answer;
+            public int Option_Length;
+        }
+        Question_Setup Question_Ref= new Question_Setup();
+
+
+        public F_Question_Template()
+        {
+            InitializeComponent();
+        }
+
+        private void F_Question_Template_Load(object sender, EventArgs e)
+        {
+            Question_Ref.Options = new List<string> (){T_Op_1.Text,T_Op_2.Text,T_Op_3.Text,T_Op_4.Text,T_Op_5.Text,T_Op_6.Text};
+            Question_Ref.Right_Answer = T_Right_Answer.Text;
+            Question_Ref.Question = T_Question.Text;
+        }
+
+        private void But_New_Question_Click(object sender, EventArgs e)
+        {
+            if (Question_Ref.Options[0] == string.Empty)
+            {
+                MessageBox.Show("ENTER at least one Option in first option slot  !!!");
+            }
+            else if (Question_Ref.Question == string.Empty)
+            {
+                MessageBox.Show("Enter a Question!!!");
+            }
+            else if (Question_Ref.Right_Answer == string.Empty) {
+                MessageBox.Show("Enter a Right Answer!!!");
+            }
+
+
+                for (int i = 0; i < Question_Ref.Options.Count; i++)
+                {
+                    if (Question_Ref.Options[i] == Question_Ref.Right_Answer)
+                    {
+                        I_Is_pair += 1;
+                    }
+                }
+
+            if(I_Is_pair > 0)
+            {
+                
+            }
+            else if(I_Is_pair == 0)
+            {
+                MessageBox.Show("Your Right Answer does not match any of your Options !!!");
+            }
+        }
+    }
+}
