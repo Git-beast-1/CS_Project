d8ea62f500e569da4d74f116b6711a341dea2098
ANDRONICUS AYO <b004724@bilborough.ac.uk>
2025-09-11 14:23:18 +0100

    Version 2

--- CS_Course_Work/Form1.Designer.cs
+++ CS_Course_Work/Form1.Designer.cs
@@ -38,81 +38,91 @@
             this.But_Create_Quiz = new System.Windows.Forms.Button();
             this.But_New_Question = new System.Windows.Forms.Button();
             this.T_Right_Answer = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.But_Left_Pan = new System.Windows.Forms.Button();
+            this.But_Right_Pan = new System.Windows.Forms.Button();
             this.T_Timer = new System.Windows.Forms.RichTextBox();
+            this.But_Tester = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // T_Question
             // 
-            this.T_Question.Location = new System.Drawing.Point(81, 71);
+            this.T_Question.Location = new System.Drawing.Point(54, 46);
+            this.T_Question.Margin = new System.Windows.Forms.Padding(2);
             this.T_Question.Name = "T_Question";
-            this.T_Question.Size = new System.Drawing.Size(530, 315);
+            this.T_Question.Size = new System.Drawing.Size(355, 206);
             this.T_Question.TabIndex = 0;
             this.T_Question.Text = "";
             // 
             // T_Op_1
             // 
-            this.T_Op_1.Location = new System.Drawing.Point(81, 426);
+            this.T_Op_1.Location = new System.Drawing.Point(54, 277);
+            this.T_Op_1.Margin = new System.Windows.Forms.Padding(2);
             this.T_Op_1.Name = "T_Op_1";
-            this.T_Op_1.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_1.Size = new System.Drawing.Size(217, 59);
             this.T_Op_1.TabIndex = 1;
             this.T_Op_1.Text = "";
             // 
             // T_Op_2
             // 
-            this.T_Op_2.Location = new System.Drawing.Point(509, 426);
+            this.T_Op_2.Location = new System.Drawing.Point(339, 277);
+            this.T_Op_2.Margin = new System.Windows.Forms.Padding(2);
             this.T_Op_2.Name = "T_Op_2";
-            this.T_Op_2.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_2.Size = new System.Drawing.Size(217, 59);
             this.T_Op_2.TabIndex = 2;
             this.T_Op_2.Text = "";
             // 
             // T_Op_3
             // 
-            this.T_Op_3.Location = new System.Drawing.Point(920, 426);
+            this.T_Op_3.Location = new System.Drawing.Point(613, 277);
+            this.T_Op_3.Margin = new System.Windows.Forms.Padding(2);
             this.T_Op_3.Name = "T_Op_3";
-            this.T_Op_3.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_3.Size = new System.Drawing.Size(217, 59);
             this.T_Op_3.TabIndex = 3;
             this.T_Op_3.Text = "";
             // 
             // T_Op_4
             // 
-            this.T_Op_4.Location = new System.Drawing.Point(81, 573);
+            this.T_Op_4.Location = new System.Drawing.Point(54, 372);
+            this.T_Op_4.Margin = new System.Windows.Forms.Padding(2);
             this.T_Op_4.Name = "T_Op_4";
-            this.T_Op_4.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_4.Size = new System.Drawing.Size(217, 59);
             this.T_Op_4.TabIndex = 4;
             this.T_Op_4.Text = "";
             // 
             // T_Op_5
             // 
-            this.T_Op_5.Location = new System.Drawing.Point(509, 573);
+            this.T_Op_5.Location = new System.Drawing.Point(339, 372);
+            this.T_Op_5.Margin = new System.Windows.Forms.Padding(2);
             this.T_Op_5.Name = "T_Op_5";
-            this.T_Op_5.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_5.Size = new System.Drawing.Size(217, 59);
             this.T_Op_5.TabIndex = 5;
             this.T_Op_5.Text = "";
             // 
             // T_Op_6
             // 
-            this.T_Op_6.Location = new System.Drawing.Point(920, 564);
+            this.T_Op_6.Location = new System.Drawing.Point(613, 367);
+            this.T_Op_6.Margin = new System.Windows.Forms.Padding(2);
             this.T_Op_6.Name = "T_Op_6";
-            this.T_Op_6.Size = new System.Drawing.Size(323, 88);
+            this.T_Op_6.Size = new System.Drawing.Size(217, 59);
             this.T_Op_6.TabIndex = 6;
             this.T_Op_6.Text = "";
             // 
             // But_Create_Quiz
             // 
-            this.But_Create_Quiz.Location = new System.Drawing.Point(679, 71);
+            this.But_Create_Quiz.Location = new System.Drawing.Point(453, 46);
+            this.But_Create_Quiz.Margin = new System.Windows.Forms.Padding(2);
             this.But_Create_Quiz.Name = "But_Create_Quiz";
-            this.But_Create_Quiz.Size = new System.Drawing.Size(177, 90);
+            this.But_Create_Quiz.Size = new System.Drawing.Size(118, 58);
             this.But_Create_Quiz.TabIndex = 10;
             this.But_Create_Quiz.Text = "Create Quiz";
             this.But_Create_Quiz.UseVisualStyleBackColor = true;
             // 
             // But_New_Question
             // 
-            this.But_New_Question.Location = new System.Drawing.Point(899, 71);
+            this.But_New_Question.Location = new System.Drawing.Point(599, 46);
+            this.But_New_Question.Margin = new System.Windows.Forms.Padding(2);
             this.But_New_Question.Name = "But_New_Question";
-            this.But_New_Question.Size = new System.Drawing.Size(177, 90);
+            this.But_New_Question.Size = new System.Drawing.Size(118, 58);
             this.But_New_Question.TabIndex = 11;
             this.But_New_Question.Text = "New Question";
             this.But_New_Question.UseVisualStyleBackColor = true;
@@ -120,46 +130,65 @@
             // 
             // T_Right_Answer
             // 
-            this.T_Right_Answer.Location = new System.Drawing.Point(509, 715);
+            this.T_Right_Answer.Location = new System.Drawing.Point(339, 465);
+            this.T_Right_Answer.Margin = new System.Windows.Forms.Padding(2);
             this.T_Right_Answer.Name = "T_Right_Answer";
-            this.T_Right_Answer.Size = new System.Drawing.Size(323, 72);
+            this.T_Right_Answer.Size = new System.Drawing.Size(217, 48);
             this.T_Right_Answer.TabIndex = 12;
             this.T_Right_Answer.Text = "Input the correct Answer";
             // 
-            // button1
+            // But_Left_Pan
             // 
-            this.button1.Location = new System.Drawing.Point(12, 344);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 105);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "L";
-            this.button1.UseVisualStyleBackColor = true;
+            this.But_Left_Pan.Location = new System.Drawing.Point(8, 224);
+            this.But_Left_Pan.Margin = new System.Windows.Forms.Padding(2);
+            this.But_Left_Pan.Name = "But_Left_Pan";
+            this.But_Left_Pan.Size = new System.Drawing.Size(42, 68);
+            this.But_Left_Pan.TabIndex = 13;
+            this.But_Left_Pan.Text = "L";
+            this.But_Left_Pan.UseVisualStyleBackColor = true;
+            this.But_Left_Pan.Click += new System.EventHandler(this.But_Left_Pan_Click);
             // 
-            // button6
+            // But_Right_Pan
             // 
-            this.button6.Location = new System.Drawing.Point(1255, 344);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(63, 105);
-            this.button6.TabIndex = 14;
-            this.button6.Text = "R";
-            this.button6.UseVisualStyleBackColor = true;
+            this.But_Right_Pan.Location = new System.Drawing.Point(837, 224);
+            this.But_Right_Pan.Margin = new System.Windows.Forms.Padding(2);
+            this.But_Right_Pan.Name = "But_Right_Pan";
+            this.But_Right_Pan.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.But_Right_Pan.Size = new System.Drawing.Size(42, 68);
+            this.But_Right_Pan.TabIndex = 14;
+            this.But_Right_Pan.Text = "R";
+            this.But_Right_Pan.UseVisualStyleBackColor = true;
+            this.But_Right_Pan.Click += new System.EventHandler(this.But_Right_Pan_Click);
             // 
             // T_Timer
             // 
-            this.T_Timer.Location = new System.Drawing.Point(679, 188);
+            this.T_Timer.Location = new System.Drawing.Point(453, 122);
+            this.T_Timer.Margin = new System.Windows.Forms.Padding(2);
             this.T_Timer.Name = "T_Timer";
-            this.T_Timer.Size = new System.Drawing.Size(177, 88);
+            this.T_Timer.Size = new System.Drawing.Size(119, 59);
             this.T_Timer.TabIndex = 15;
             this.T_Timer.Text = "Enter Time in seconds";
             // 
+            // But_Tester
+            // 
+            this.But_Tester.Location = new System.Drawing.Point(625, 455);
+            this.But_Tester.Margin = new System.Windows.Forms.Padding(2);
+            this.But_Tester.Name = "But_Tester";
+            this.But_Tester.Size = new System.Drawing.Size(118, 58);
+            this.But_Tester.TabIndex = 16;
+            this.But_Tester.Text = "Bug_Tester";
+            this.But_Tester.UseVisualStyleBackColor = true;
+            this.But_Tester.Click += new System.EventHandler(this.But_Tester_Click);
+            // 
             // F_Question_Template
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1330, 901);
+            this.ClientSize = new System.Drawing.Size(887, 586);
+            this.Controls.Add(this.But_Tester);
             this.Controls.Add(this.T_Timer);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.But_Right_Pan);
+            this.Controls.Add(this.But_Left_Pan);
             this.Controls.Add(this.T_Right_Answer);
             this.Controls.Add(this.But_New_Question);
             this.Controls.Add(this.But_Create_Quiz);
@@ -170,6 +199,7 @@
             this.Controls.Add(this.T_Op_2);
             this.Controls.Add(this.T_Op_1);
             this.Controls.Add(this.T_Question);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "F_Question_Template";
             this.Text = "Question Template";
             this.Load += new System.EventHandler(this.F_Question_Template_Load);
@@ -189,9 +219,10 @@
         private System.Windows.Forms.Button But_Create_Quiz;
         private System.Windows.Forms.Button But_New_Question;
         private System.Windows.Forms.RichTextBox T_Right_Answer;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button But_Left_Pan;
+        private System.Windows.Forms.Button But_Right_Pan;
         private System.Windows.Forms.RichTextBox T_Timer;
+        private System.Windows.Forms.Button But_Tester;
     }
 }
 
--- CS_Course_Work/Form1.cs
+++ CS_Course_Work/Form1.cs
@@ -9,22 +9,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CS_Course_Work
+namespace CS_Course_Work // NEXT UP, COMPLETE THE PAN INTERFACE AND SEPERATE THE STRING OPTIONS BACK IN THE PAN FUNCTIONS 
 {
     public partial class F_Question_Template : Form
     {
-            int I_Is_pair;
-        public class Question_Setup
+        int I_Is_pair,Q_index;// I_is_pair:: for comparing the right and options value 
+                              // Q_index :: this holds the index value of the "Complete_Question_setup", it is used for panning around 
+
+        public class Question_Setup// this class stores a list of different kind of data needed for the full questions
         {
             public string Question;
-            public List<string> Options;
+            public string Options;
             public string Right_Answer;
-            public int Option_Length;
         }
-        Question_Setup Transfer_Q_to_List = new Question_Setup();
-        List<Question_Setup> Complete_Question_setup = new List<Question_Setup>();
-
-        public List<RichTextBox> Current_Options;
+        List<Question_Setup> Complete_Question_setup = new List<Question_Setup>();// holds a list of question of type "Question_Setup"
+        public List<RichTextBox> Current_Options;// responsible for the current options in the interface 
 
 
         public F_Question_Template()
@@ -34,50 +33,95 @@ namespace CS_Course_Work
 
         private void F_Question_Template_Load(object sender, EventArgs e)
         {
-           Current_Options = new List<RichTextBox> (){T_Op_1,T_Op_2,T_Op_3,T_Op_4,T_Op_5,T_Op_6};
+            Current_Options = new List<RichTextBox>() { T_Op_1, T_Op_2, T_Op_3, T_Op_4, T_Op_5, T_Op_6 };// when the form loads, all the option boxes are assigned to "Current_Options"
+            Q_index = 0;// the index starts off at zero for the first question and will progressively increase for each questions 
         }
 
         private void But_New_Question_Click(object sender, EventArgs e)
         {
-            if (T_Question.Text == string.Empty || T_Right_Answer.Text == string.Empty)
+            if (T_Question.Text == string.Empty || T_Right_Answer.Text == string.Empty) // Checkes if the Question Box has been filled  and returns a message if false; 
             {
-                MessageBox.Show("Your Question or Option slot is empty, please fill it ");
+                MessageBox.Show("Your Question,Right Answer slot  or Option slot is empty, please fill it,MAKE SURE SPACE ISN'T INCLUDED AT THE END OF YOUR OPTION INPUT ");
             }
-            else if (Current_Options[0].Text == string.Empty)
+            else if (Current_Options[0].Text == string.Empty) // checks if the first option slot has been filled and returns a message if false 
             {
-                MessageBox.Show("Make sure fill in option box, the first option slot should be filled ");
+                MessageBox.Show("Make sure fill in option box, the first option slot should be filled,MAKE SURE SPACE ISN'T INCLUDED AT THE END OF YOUR OPTION INPUT !!! ");
             }
             else
             {
-                for (int i = 0; i < Current_Options.Count; i++)
+            for (int i = 0; i < Current_Options.Count; i++)
                 {
-                    if (Current_Options[i].Text == T_Right_Answer.Text)
+                    if (Current_Options[i].Text == T_Right_Answer.Text)// Compares all the option values in the option box to the right answer input 
                     {
                         I_Is_pair += 1;
                     }
                 }
 
-                if (I_Is_pair == 1)
+                if (I_Is_pair == 1) // if one of the options matches the right answer, it saves the question, options and right answer data to a  temporary record structure of the Question setup 
                 {
-                    string Convert_List_to_String;
+                    Question_Setup Temp_Q_setup = new Question_Setup(); 
+                    Temp_Q_setup.Question = T_Question.Text;
+                    Temp_Q_setup.Right_Answer = T_Right_Answer.Text;
 
-                    Transfer_Q_to_List.Question = T_Question.Text;
-                    Transfer_Q_to_List.Right_Answer= T_Right_Answer.Text;
+                    string Compress_to_Line;       
+                    List<string> List_to_string = new List<string>();// Takes the text value of the option lists and converts it to a list of strings; 
 
-                    //Up next Saving Options to Structure
+                    for (int i = 0; i < Current_Options.Count; i++)
+                    {
+                        if (Current_Options[i].Text == string.Empty)// if one of the option boxes are empty and have no value in it, the "continue" key word skips the current iteration of the loop
+                        {                                           //hence not adding it to the list of strings 
+                            continue;
+                        }
+                        List_to_string.Add(Current_Options[i].Text);
 
+                    }
+                    Compress_to_Line = string.Join(",", List_to_string);
+                    Temp_Q_setup.Options = Compress_to_Line;
+
+                   Complete_Question_setup.Add(Temp_Q_setup);
+                    Reset_Question();// resets the Ui values for another question input 
                 }
-                else if (I_Is_pair !=  1)
+                else if (I_Is_pair != 1)// if none of the options are thesame with the right answer or more than one of them are thesame, it returns message and doesn't save the data
                 {
                     MessageBox.Show("Some of your Options are thesame  OR dont match the right answer input ");
                 }
 
             }
-            I_Is_pair = 0;
+             I_Is_pair = 0;
         }
-        public void Outer_the_Way()
+
+
+
+          void Reset_Question()
+         {
+            // in the reset functions, all the values are set to empty, to completely wipe all their string values 
+            T_Question.Text = string.Empty;
+            T_Right_Answer.Text = string.Empty;
+            for(int i = 0;i < Current_Options.Count; i++)
+            {
+                Current_Options[i].Text = string.Empty;
+            }
+
+           }
+
+        private void But_Right_Pan_Click(object sender, EventArgs e)
         {
-       
+            List<string> List_to_string = new List<string>();
+            Q_index += 1;
+
+            T_Question.Text = Complete_Question_setup[Q_index].Question;
+            T_Right_Answer.Text = Complete_Question_setup[Q_index].Right_Answer;
+          //  List_to_string = Complete_Question_setup[Q_index].Options.Split();
         }
-    }
+
+        private void But_Left_Pan_Click(object sender, EventArgs e)
+        {
+            List<string> List_to_string = new List<string>();
+        }
+
+        private void But_Tester_Click(object sender, EventArgs e)
+        {
+
+        }
+    } 
 }
